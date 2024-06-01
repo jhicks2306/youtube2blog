4e8b0768870e3d9e259681cf8f6bc6c1aa8c881e
jhicks2306 <jhicks2306@gmail.com>
2024-06-01 22:45:38 +0100

    Add tabs for Blog view

--- app/[id]/outline/card.tsx
+++ app/[id]/outline/card.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from '@/components/ui/button';
 import { VideoData } from '@/lib/definitions';
 import TranscriptBottomSheet from './bottomsheet';
@@ -33,21 +34,32 @@ export default async function OutlineCard({ video }: { video: VideoData }) {
 
   return (
     <>
-      <Card className="flex flex-col w-full grow">
+      <Card className="flex flex-col w-full grow min-h-full">
         <CardHeader>
           <CardTitle> Edit Outline</CardTitle>
           <CardDescription>Edit your blog outline</CardDescription>
         </CardHeader>
         <CardContent className='flex flex-col grow'>
-            <Textarea className='grow' placeholder='Outline for blog goes here...' defaultValue={video.outline}/>                
-            <div className='flex flex-row'>
-                <div className='order-last'>
-                  <Link href='/blog'>
-                    <Button className='mt-4 w-fit'>Generate blog</Button>
-                  </Link>
-                </div>
-                <TranscriptBottomSheet video={video}/>
-            </div>
+          <Tabs defaultValue="outline" className="flex flex-col">
+            <TabsList className='grid w-full grid-cols-2'>
+              <TabsTrigger value="outline">Outline</TabsTrigger>
+              <TabsTrigger value="blog">Blog</TabsTrigger>
+            </TabsList>
+            <TabsContent value="outline" className='data-[state="active"]:flex data-[state="active"]:grow'>
+              <Textarea className='' placeholder='Outline for blog goes here...' defaultValue={video.outline}/>                
+            </TabsContent>
+            <TabsContent value="blog" className='data-[state="active"]:flex data-[state="active"]:grow'>
+              <Textarea className='' placeholder='Blog goes here...' />
+            </TabsContent>
+          </Tabs>                
+          <div className='flex flex-row'>
+              <div className='order-last'>
+                <Link href='/blog'>
+                  <Button className='mt-4 w-fit'>Generate blog</Button>
+                </Link>
+              </div>
+              <TranscriptBottomSheet video={video}/>
+          </div>
         </CardContent>
       </Card>
     </>
